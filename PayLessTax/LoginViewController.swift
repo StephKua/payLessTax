238e2031720572a72fbdf1fe8f5e9a00f79dee9f
sheena <sheenamoh@gmail.com>
2016-07-19 17:00:43 +0800

    Initial Commit

---
+++ PayLessTax/LoginViewController.swift
@@ -0,0 +1,21 @@
+//
+//  ViewController.swift
+//  PayLessTax
+//
+//  Created by Sheena Moh on 18/07/2016.
+//  Copyright © 2016 SMoh. All rights reserved.
+//
+
+import UIKit
+
+class LoginViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+    }
+
+    
+    
+}
+
