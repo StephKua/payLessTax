bb3306ed21e57befe08cd864b1e537e3503298fc
Skkz <Me@Skkzs-MacBook-Air.local>
2016-07-20 11:22:42 +0800

    add rebate

--- PayLessTax/NewRebateViewController.swift
+++ PayLessTax/NewRebateViewController.swift
@@ -7,18 +7,45 @@
 //
 
 import UIKit
+import Firebase
 
 class NewRebateViewController: UIViewController {
-
+    
+    var selectedRebate: Rebate
+    let firebaseRef = FIRDatabase.database().reference()
+    
+    @IBOutlet weak var dateTextField: UITextField!
+    
+    @IBOutlet weak var amountTextField: UITextField!
+    @IBOutlet weak var receiptTextField: UITextField!
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-
+        self.title = selectedRebate.rebateName
         // Do any additional setup after loading the view.
     }
 
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
+    }
+    
+    func addRebate() {
+        let rebateRef = firebaseRef.child("Rebate").child(User.useruid()).child(selectedRebate)
+        
+        guard let dateTextField != nil, let amountTextField != nil else {return }
+        
+        let addedAmount = amountTextField.text as? Int
+        let subtotal = selectedRebate.rebatePoints + addedAmount
+        let rebateDict = ["subtotal": subtotal]
+        rebateRef.setValue(rebateDict)
+        
+        let receiptRef = firebaseRef.child("Receipt").childByAutoId()
+        let receiptDict = ["date": dateTextField.text, "amount": addedAmount, "receipt no": receiptTextField.text]
+        receiptRef.setValue(receiptDict)
+    
+        
     }
     
 
--- PayLessTax/Rebate.swift
+++ PayLessTax/Rebate.swift
@@ -7,3 +7,40 @@
 //
 
 import Foundation
+import Firebase
+
+class Rebate {
+    var rebateName = String()
+    var rebatePoints = Int()
+    var rebateMaxPoints = 1000
+    var subTotal = 0
+    
+    init?(snapshot: FIRDataSnapshot) {
+        guard let rebateDict = snapshot.value as? [String: AnyObject] else {
+            return nil
+        }
+        
+        if let name = rebateDict["rebateName"] as? String {
+            self.rebateName = name
+        }
+        
+        if let point = rebateDict["rebatePoints"] as? Int {
+            if point == 0 {
+                self.rebatePoints = 0
+            } else {
+                self.rebatePoints = point
+            }
+        }
+        
+        if let subTotal = rebateDict["subtotal"] as? Int {
+            if subTotal == 0 {
+                self.subTotal = 0
+            } else {
+                self.subTotal += subTotal
+            }
+        }
+        
+    }
+    
+    
+}
