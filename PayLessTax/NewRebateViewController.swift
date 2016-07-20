4b44cd0c6313e4d0497ba215d6d9994c9f89a0f8
Skkz <Me@Skkzs-MacBook-Air.local>
2016-07-20 11:54:00 +0800

    added rebate

--- PayLessTax/NewRebateViewController.swift
+++ PayLessTax/NewRebateViewController.swift
@@ -6,19 +6,65 @@
 //  Copyright © 2016 SMoh. All rights reserved.
 //
 
+//
+//  NewRebateViewController.swift
+//  PayLessTax
+//
+//  Created by Sheena Moh on 19/07/2016.
+//  Copyright © 2016 SMoh. All rights reserved.
+//
+
 import UIKit
+import Firebase
 
 class NewRebateViewController: UIViewController {
-
+    
+    var selectedRebate: Rebate
+    let firebaseRef = FIRDatabase.database().reference()
+    
+    @IBOutlet weak var dateTextField: UITextField!
+    
+    @IBOutlet weak var amountTextField: UITextField!
+    @IBOutlet weak var receiptTextField: UITextField!
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-
+        self.title = selectedRebate.rebateName
         // Do any additional setup after loading the view.
     }
-
-    @IBAction func onSaveBtnPressed(sender: UIBarButtonItem) {
+    
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
     }
     
+    func addRebate() {
+        let rebateRef = firebaseRef.child("Rebate").child(User.useruid()).child(selectedRebate)
+        
+        guard let dateTextField != nil, let amountTextField != nil else {return }
+        
+        let addedAmount = amountTextField.text as? Int
+        let subtotal = selectedRebate.rebatePoints + addedAmount
+        let rebateDict = ["subtotal": subtotal]
+        rebateRef.setValue(rebateDict)
+        
+        let receiptRef = firebaseRef.child("Receipt").childByAutoId()
+        let receiptDict = ["date": dateTextField.text, "amount": addedAmount, "receipt no": receiptTextField.text]
+        receiptRef.setValue(receiptDict)
+        
+        
+    }
     
+    /*
+     // MARK: - Navigation
+     
+     // In a storyboard-based application, you will often want to do a little preparation before navigation
+     override func prepareForSegue(segue: UIStoryboardSegue, sender: AnyObject?) {
+     // Get the new view controller using segue.destinationViewController.
+     // Pass the selected object to the new view controller.
+     }
+     */
     
 }
+
--- PayLessTax/RebateTableViewController.swift
+++ PayLessTax/RebateTableViewController.swift
@@ -6,88 +6,65 @@
 //  Copyright © 2016 SMoh. All rights reserved.
 //
 
+
+
 import UIKit
 import Firebase
 
 class RebateTableViewController: UITableViewController {
-
+    
+    var listOfRebates = [Rebate]()
+    let firebaseRef = FIRDatabase.database().reference()
+    var listOfImages = [UIImage]()
+    var typesOfRebates = [String]()
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-
-        // Uncomment the following line to preserve selection between presentations
-        // self.clearsSelectionOnViewWillAppear = false
-
-        // Uncomment the following line to display an Edit button in the navigation bar for this view controller.
-        // self.navigationItem.rightBarButtonItem = self.editButtonItem()
+        self.title = "Get Rebates"
+        getRebate()
+        self.typesOfRebates = ["Books", "Donations", "Sports"]
+        self.tableView.reloadData()
+        
     }
-
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
-    }
-
+    
     // MARK: - Table view data source
-
-
-
+    
     override func tableView(tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
         // #warning Incomplete implementation, return the number of rows
-        return 0
+        return self.listOfRebates.count
     }
-
-  
+    
+    
     override func tableView(tableView: UITableView, cellForRowAtIndexPath indexPath: NSIndexPath) -> UITableViewCell {
-        let cell = tableView.dequeueReusableCellWithIdentifier("RebateCell", forIndexPath: indexPath)
-
-        cell.textLabel?.text = "rebates"
-
+        let cell = tableView.dequeueReusableCellWithIdentifier("rebatecell", forIndexPath: indexPath) as! RebateTableViewCell
+        let rebate = self.listOfRebates[indexPath.row]
+        rebate.rebateName = self.typesOfRebates[indexPath.row]
+        cell.rebateLabel.text = self.typesOfRebates[indexPath.row]
+        cell.pointLabel.text = "RM \(rebate.rebatePoints)"
+        cell.maxPointLabel.text = "RM \(rebate.rebateMaxPoints)"
+        cell.rebateImageView.image = self.listOfImages[indexPath.row]
         return cell
     }
     
-
-    /*
-    // Override to support conditional editing of the table view.
-    override func tableView(tableView: UITableView, canEditRowAtIndexPath indexPath: NSIndexPath) -> Bool {
-        // Return false if you do not want the specified item to be editable.
-        return true
+    
+    @IBAction func logOutBtnClicked(sender: UIBarButtonItem) {
+        try! FIRAuth.auth()!.signOut()
+        // User.removeUserUid()
     }
-    */
-
-    /*
-    // Override to support editing the table view.
-    override func tableView(tableView: UITableView, commitEditingStyle editingStyle: UITableViewCellEditingStyle, forRowAtIndexPath indexPath: NSIndexPath) {
-        if editingStyle == .Delete {
-            // Delete the row from the data source
-            tableView.deleteRowsAtIndexPaths([indexPath], withRowAnimation: .Fade)
-        } else if editingStyle == .Insert {
-            // Create a new instance of the appropriate class, insert it into the array, and add a new row to the table view
-        }    
+    
+    func getRebate() {
+        let rebateRef = firebaseRef.child("Rebate").child(User.useruid())
+        rebateRef.observeEventType(.ChildAdded, withBlock:  { (snapshot) in
+            if let rebate = Rebate(snapshot: snapshot) {
+                self.listOfRebates.append(rebate)
+            }
+        })
     }
-    */
-
-    /*
-    // Override to support rearranging the table view.
-    override func tableView(tableView: UITableView, moveRowAtIndexPath fromIndexPath: NSIndexPath, toIndexPath: NSIndexPath) {
-
+    
+    override func prepareForSegue(segue: UIStoryboardSegue, sender: AnyObject){
+        let dest = segue.destinationViewController as! NewRebateViewController
+        let indexPath = self.tableView.indexPathForSelectedRow
+        let rebate = self.listOfRebates[(indexPath?.row)!]
+        dest.selectedRebate = rebate
     }
-    */
-
-    /*
-    // Override to support conditional rearranging of the table view.
-    override func tableView(tableView: UITableView, canMoveRowAtIndexPath indexPath: NSIndexPath) -> Bool {
-        // Return false if you do not want the item to be re-orderable.
-        return true
-    }
-    */
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepareForSegue(segue: UIStoryboardSegue, sender: AnyObject?) {
-        // Get the new view controller using segue.destinationViewController.
-        // Pass the selected object to the new view controller.
-    }
-    */
-
 }
