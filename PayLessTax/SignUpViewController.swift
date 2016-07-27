82c925e88f3e1a0f2893cdef00b4dc6be7808bbe
sheena <sheenamoh@gmail.com>
2016-07-27 14:42:06 +0800

    added picker view to new rebate and new income

--- PayLessTax/SignUpViewController.swift
+++ PayLessTax/SignUpViewController.swift
@@ -9,22 +9,32 @@
 import UIKit
 import Firebase
 
-class SignUpViewController: UIViewController {
+class SignUpViewController: UIViewController, UITextFieldDelegate {
 
     @IBOutlet weak var usernameTextField: UITextField!
     @IBOutlet weak var emailTextField: UITextField!
     @IBOutlet weak var incomeTaxNoTextField: UITextField!
     @IBOutlet weak var passwordTextField: UITextField!
 
+    //add iboutlet fr scroll view
+    var activeTextField: UITextField?
     
     var firebaseRef = FIRDatabase.database().reference()
     
     
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        self.hideKeyboardWhenTapped()
+    
+    }
+    
+    override func viewWillAppear(animated: Bool) {
+        super.viewWillAppear(animated)
+//        self.registerForKeyboardNotifications()
+    }
     
     @IBAction func onSignUpBtnClicked(sender: UIButton) {
         guard let username = usernameTextField.text, let email = emailTextField.text, let password = passwordTextField.text, let incomeTaxNo = incomeTaxNoTextField.text else { return }
-        
-
         
         FIRAuth.auth()?.createUserWithEmail(email, password: password, completion: { (user, error) in
             if let user = user {
@@ -49,15 +59,43 @@ class SignUpViewController: UIViewController {
         })
     }
     
-    override func viewDidLoad() {
-        super.viewDidLoad()
-
-        self.hideKeyboardWhenTapped()
+    /*
+    
+    func registerForKeyboardNotifications() {
+        NSNotificationCenter.defaultCenter().addObserver(self, selector: #selector(keyboardWasShown), name: UIKeyboardWillShowNotification, object: nil)
+    }
+    
+    func keyboardWasShown(notification: NSNotification) {
+        let info = notification.userInfo!    //userinfo tells u info incl keyboard size (info tat notification sends)
+        let kbSize = (info[UIKeyboardFrameBeginUserInfoKey] as? NSValue)?.CGRectValue().size    //obtain keyboard frame N size
         
-        // Do any additional setup after loading the view.
+        let contentInsets = UIEdgeInsetsMake(0.0, 0.0, kbSize!.height, 0.0)
+        scrollView.contentInset = contentInsets //need too add iboutlet
+        scrollView.scrollIndicatorInsets = contentInsets
+        
+        var varrect = self.view.frame //getting frame of whole view
+        rect.size.height -= kbSize!.height
+        
+        if let activeField = activeTextField {
+            if CGRectContainsPoint(rect, activeTextField?.frame.origin) {
+                self.scrollView.scrollRectToVisible(activeField.frame, animated: true)
+
+            }
+        }
+    }
+ 
+ */
+    
+    
+    func textFieldDidBeginEditing(textField: UITextField) {
+        activeTextField = textField
     }
 
-   
+    func textFieldDidEndEditing(textField: UITextField) {
+        activeTextField = nil
+    }
+
+    
 }
 
 extension UIViewController {
